ca925547dcb35e2f54c9ffd70f7cfd5e738ecdc5
Matt <mkovars@wisc.edu>
2018-09-02 16:45:22 -0500

    More advanced mapping.
    Use the github repo of the CMU dict as a submodule instead of copying
    the dictionary file. Future revisions of the transformation logic should
    now mostly take place in transformation.json. Clean up some of the text.

--- README.md
+++ README.md
@@ -6,6 +6,26 @@ Software to approximately transform words of Orthodox English Spelling into Quic
 This is not a true dictionary, but rather a phonetic transformation from english
 words to Quickscript. Some standardized spellings may be transformed incorrectly.
 
-The character 34-Ox will not appear in any of the transformations. This is
-because this page uses the Carnegie Mellon Pronouncing Dictionary to lookup the
-pronunciation of words, and the ARPABET does not have a phoneme that maps to Ox.
+# How it works
+
+## Manually transforming words
+If the word matches any of the words in the wordReplace array in `transformation.js`,
+the spellings provided are displayed to the user. The page then continues to attempt
+to tranform the word phonetically.
+
+## Phonetically transforming words
+The page checks for an entry of the word in the CMU Pronunciation dictionary
+(`cmudict/cmudict.dict`). The dictionary outputs
+[phonetic symbols](https://en.wikipedia.org/wiki/ARPABET) that represent how the
+word is pronounced.
+
+Those phonetic symbols are then mapped to Quickscript characters. This mapping is
+usually fairly simple because Quickscript characters are mostly phonetic. However,
+there are some additional rules to help handle edge cases. This phonetic mapping
+is specified in `transformation.js`. The transformation first runs the regex
+replacements specified in `regexReplace`, and then runs the plain phonetic
+replacements specified in `plainReplace`.
+
+# Open Source projects used
+[The Carnegie Mellon Pronouncing Dictionary](https://github.com/cmusphinx/cmudict)
+[The "Abbots Morton Experiment" font for Quickscript](https://github.com/adiabatic/abbots-morton-experiment)
--- qs-dict.js
+++ qs-dict.js
@@ -13,124 +13,226 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+const dictURL = "cmudict/cmudict.dict?v=2";
+const transformURL = "transformation.json?v=2";
 
-let CMUdict = {};
+let CMUdict = new Map();
+let wordReplace = new Map();
+let phenomeReplace = [];
+
+let retrieveStat = {};
 window.addEventListener("load",pageLoad);
 
 function pageLoad() {
-    let dictReq = new XMLHttpRequest();
-    let inputElem = document.getElementById("word-input");
-    let buttonElem = document.getElementById("word-button");
 
-    
-    dictReq.addEventListener("progress", function(evt) {
-        console.log("progress " + evt.loaded);
-        if (evt.lengthComputable) {
-            let percentComplete = (evt.loaded / evt.total);
-            document.getElementById("progress").value = percentComplete;
-        }
-    });
+    // Request the CMU dictionary
+    let dictReq = new XMLHttpRequest();
+    dictReq.addEventListener("progress", generateProgressHandler("dict-progress"));
     dictReq.addEventListener("readystatechange", function() {
         if (dictReq.readyState === 4) {
-            let statusElem = document.getElementById("status");
+            let loaded = false;
             if (dictReq.status === 200) {
-
-                // construct the CMUdict dictionary
-
-                let lines = dictReq.responseText.trim().split("\n");
-
-                for (let line of lines) {
-                    let re = /^(.*?)(?:\([0-9]+\))?\t(.*)$/;
-                    let matches = re.exec(line);
-                    if (matches) {
-                        let word = matches[1];
-                        let phonemes = matches[2];
-                        
-                        let wordEntry = CMUdict[word] || [];
-                        wordEntry.push(phonemes);
-                        CMUdict[word] = wordEntry;
-                    } else {
-                        console.log("line does not match: " + line);
-                    }
+                try {
+                    // Construct the CMUdict dictionary.
+                    saveDictionary(dictReq.responseText);
+                    retrieveDone("dict");
+                    loaded = true;
+                } catch(e) {
+                    console.log(e);
                 }
 
-                // Setup the HTML elements to respond to input
+            }
 
-                inputElem.addEventListener("keydown",function(event){
-                    let e = event || window.event;
-                    if(e.keyCode == 13){
-                        submitWord();
-                    }
-                });
-                buttonElem.addEventListener("click", function(){
-                    submitWord();
-                });
-                inputElem.disabled=false;
-                statusElem.innerText = "Dictionary Loaded.";
+            let dictStatusElem = document.getElementById("dict-status");
+            if (loaded) {
+                dictStatusElem.innerText = "Dictionary Loaded.";
             } else {
-                statusElem.innerText = "Dictionary failed to load";
+                dictStatusElem.innerText = "Dictionary failed to load";
             }
         }
     });
-    dictReq.open("GET","cmudict/cmudict.0.7a_SPHINX_40", true);
+
+    dictReq.open("GET",dictURL, true);
+    dictReq.responseType = "text";
     dictReq.send();
+
+
+    // Request the transformation json file
+    let transReq = new XMLHttpRequest();
+    transReq.addEventListener("progress", generateProgressHandler("transform-progress"));
+    transReq.addEventListener("readystatechange", function() {
+        if (transReq.readyState === 4) {
+            let loaded = false;
+            if (transReq.status === 200) {
+                // Construct the CMUdict dictionary.
+                try {
+                    saveTransform(transReq.response);
+                    retrieveDone("trans");
+                    loaded = true;
+                } catch(e) {
+                    console.log(e);
+                }
+
+            }
+
+            let dictStatusElem = document.getElementById("transform-status");
+            if (loaded) {
+                dictStatusElem.innerText = "Transform Loaded.";
+            } else {
+                dictStatusElem.innerText = "Transform failed to load";
+            }
+        }
+    });
+    transReq.open("GET",transformURL, true);
+    transReq.responseType = "json";
+    transReq.send();
 }
 
-let phenomeMap = {
-    "AA": "\uE676", // Ah
-    "AE": "\uE674", // At
-    "AH": "\uE67A", // Utter
-    "AO": "\uE677", // Awe
-    "AW": "\uE67B", // Out
-    "AY": "\uE675", // I
-    "B":  "\uE651", // Bay
-    "CH": "\uE65E", // Cheer
-    "D":  "\uE653", // Day
-    "DH": "\uE657", // They
-    "EH": "\uE672", // Et
-    "ER": "\uE67A\uE668", // Utter-Roe
-    "EY": "\uE673", // Eight
-    "F":  "\uE658", // Fee
-    "G":  "\uE655", // Gay
-    "HH": "\uE662", // He
-    "IH": "\uE670", // It
-    "IY": "\uE671", // Eat
-    "JH": "\uE65F", // Jay
-    "K":  "\uE654", // Key
-    "L":  "\uE667", // Low
-    "M":  "\uE665", // May
-    "N":  "\uE666", // No
-    "NG": "\uE664", // Ing
-    "OW": "\uE67C", // Owe
-    "OY": "\uE679", // Oy
-    "P":  "\uE650", // Pea
-    "R":  "\uE668", // Roe
-    "S":  "\uE65A", // See
-    "SH": "\uE65C", // She
-    "T":  "\uE652", // Tea
-    "TH": "\uE656", // Thaw
-    "UH": "\uE67D", // Foot
-    "UW": "\uE67E", // Ooze
-    "V":  "\uE659", // Vie
-    "W":  "\uE661", // Way
-    "WH": "\uE663", // Why
-    "Y":  "\uE660", // Ye
-    "Z":  "\uE65B", // Zoo
-    "ZH": "\uE65D", // J'ai
+function retrieveDone(reqID) {
+    retrieveStat[reqID] = true;
+
+    if (retrieveStat["dict"] && retrieveStat["trans"]) {
+        let inputElem = document.getElementById("word-input");
+        let buttonElem = document.getElementById("word-button");
+
+        // Setup the HTML elements to respond to input.
+        inputElem.addEventListener("keydown",function(event){
+            let e = event || window.event;
+            if(e.keyCode == 13){
+                submitWord();
+            }
+        });
+        buttonElem.addEventListener("click", function(){
+            submitWord();
+        });
+        inputElem.disabled=false;
+    }
+}
+
+function generateProgressHandler(elemID) {
+    let progressElem = document.getElementById(elemID);
+
+    return function(evt) {
+        if (evt.lengthComputable) {
+            let percentComplete = (evt.loaded / evt.total);
+            progressElem.value = percentComplete;
+        }
+    };
+}
+
+function saveDictionary(dictText) {
+    let lines = dictText.trim().split("\n");
+
+    for (let line of lines) {
+        let re = /^(.*?)(?:\([0-9]+\))?\s(.*)$/;
+        let matches = re.exec(line);
+        if (matches) {
+            let word = matches[1];
+            let phonemes = matches[2];
+            
+            let wordEntry = CMUdict.get(word) || [];
+            try {
+            wordEntry.push(phonemes);
+            }catch (e) {
+                console.log(e);
+            }
+            CMUdict.set(word, wordEntry);
+        } else {
+            console.log("line does not match: " + line);
+        }
+    }
+}
+
+let qsm = { // Quickscript map
+    "pea":   "\uE650",
+    "bay":   "\uE651",
+    "tea":   "\uE652",
+    "day":   "\uE653",
+    "key":   "\uE654",
+    "gay":   "\uE655",
+    "thaw":  "\uE656",
+    "they":  "\uE657",
+    "fee":   "\uE658",
+    "vie":   "\uE659",
+    "see":   "\uE65A",
+    "zoo":   "\uE65B",
+    "she":   "\uE65C",
+    "j'ai":  "\uE65D",
+    "cheer": "\uE65E",
+    "jay":   "\uE65F",
+    "ye":    "\uE660",
+    "way":   "\uE661",
+    "he":    "\uE662",
+    "why":   "\uE663",
+    "ing":   "\uE664",
+    "may":   "\uE665",
+    "no":    "\uE666",
+    "low":   "\uE667",
+    "roe":   "\uE668",
+    "it":    "\uE670",
+    "eat":   "\uE671",
+    "et":    "\uE672",
+    "eight": "\uE673",
+    "at":    "\uE674",
+    "i":     "\uE675",
+    "ah":    "\uE676",
+    "awe":   "\uE677",
+    "ox":    "\uE678",
+    "oy":    "\uE679",
+    "utter": "\uE67A",
+    "out":   "\uE67B",
+    "owe":   "\uE67C",
+    "foot":  "\uE67D",
+    "ooze":  "\uE67E",
 };
+
+function convertNamesToQS(str) {
+    return str.split(" ").map(name => qsm[name] || "?").join("");
+}
+
+function saveTransform(transJSON) {
+    // Load the direct English -> Quickscript translations.
+    for (let replace of transJSON["wordReplace"]) {
+        let qsWords = replace[1].map(convertNamesToQS);
+        wordReplace.set(replace[0], qsWords);
+    }
+
+    // Load the regex transformations.
+    for (let replace of transJSON["regexReplace"]) {
+        let re = new RegExp(replace[0], replace[1]);
+        phenomeReplace.push([re, convertNamesToQS(replace[2])]);
+    }
+
+    // Load the plain phenome transformations.
+    for (let replace of transJSON["plainReplace"]) {
+        let re = new RegExp("\\b" + replace[0] + "\\b", "g");
+        phenomeReplace.push([re, convertNamesToQS(replace[1])]);
+    }
+}
 
 function submitWord() {
     let inputElem = document.getElementById("word-input");
     let phenomElem = document.getElementById("phenomes-output");
-    let qsElem = document.getElementById("quickscript-output");
+    let phenomQSElem = document.getElementById("phenomeQS-output");
+    let manualElem = document.getElementById("manual-output");
 
-    let word = inputElem.value.toUpperCase();
+    let word = inputElem.value.toLowerCase();
 
-    let phenomes = CMUdict[word];
+    let manualWords = wordReplace.get(word);
 
-    if (!phenomes) {
+    if (manualWords) {
+        manualElem.innerText = manualWords.join("\n");
+        manualElem.style.display = "inherit";
+    } else {
+        manualElem.innerText = "";
+        manualElem.style.display = "none";
+    }
+
+    let phenomes = CMUdict.get(word);
+
+    if (!phenomes && !manualWords) {
         phenomElem.innerText = "<Word not found>";
-        qsElem.innerText = "";
+        phenomQSElem.innerText = "";
         return;
     }
 
@@ -139,18 +241,14 @@ function submitWord() {
     let qsTranscripts = [];
 
     for (let phenomeStr of phenomes) {
-        // The CMU phonetic dictionary stores the Why sound as HH W
-        phenomeStr = " " + phenomeStr + " ";
-        phenomeStr = phenomeStr.replace(" HH W "," WH ").trim();
-        
-        let qsStr = "";
-        for (let phenome of phenomeStr.split(" ")) {
-            let qsChar = phenomeMap[phenome] || "?";
-            qsStr += qsChar;
+        // Transform the phenomes in order.
+        for (let transform of phenomeReplace) {
+            phenomeStr = phenomeStr.replace(transform[0], transform[1]);
         }
+        phenomeStr = phenomeStr.replace(/ /g,"").trim();
         
-        qsTranscripts.push(qsStr);
+        qsTranscripts.push(phenomeStr);
     }
 
-    qsElem.innerText = qsTranscripts.join("\n");
+    phenomQSElem.innerText = qsTranscripts.join("\n");
 }
